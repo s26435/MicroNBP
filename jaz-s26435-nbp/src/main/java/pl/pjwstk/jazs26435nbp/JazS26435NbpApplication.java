33d8d24499d44f94d02887020d41a805e2afd5a8
s26435 <92215037+s26435@users.noreply.github.com>
2023-06-24 15:09:55 +0200

    initial commit

---
+++ jaz-s26435-nbp/src/main/java/pl/pjwstk/jazs26435nbp/JazS26435NbpApplication.java
@@ -0,0 +1,13 @@
+package pl.pjwstk.jazs26435nbp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JazS26435NbpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JazS26435NbpApplication.class, args);
+    }
+
+}
---
+++ jaz-s26435-nbp/src/test/java/pl/pjwstk/jazs26435nbp/JazS26435NbpApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pjwstk.jazs26435nbp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JazS26435NbpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
